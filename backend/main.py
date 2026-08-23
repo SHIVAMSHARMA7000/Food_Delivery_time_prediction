4544cde879ac1c92f0143ac198a743188c46e719
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-08-23 11:01:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class DeliveryInput(BaseModel):
 
